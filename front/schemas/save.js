4e9263b1b110c946a46631dfaa6bcdf20992b3c9
Ahmet Kaptan <38915226+codescaptain@users.noreply.github.com>
2022-01-07 19:49:45 +0300

    add a front-side

---
+++ front/schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'save',
+  title: 'Save',
+  type: 'document',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'PostedBy',
+      type: 'postedby'
+    },
+    {
+      name: 'userId',
+      title: 'userId',
+      type: 'string'
+    },
+  ]
+}
